bc79013f784556bb22538258f50ed7615b928fc3
jakob willfors <Jakob.Willfors@medieinstitutet.se>
2026-03-30 23:17:14 +0200

    fix cors for lovable

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 class GenerateRequest(BaseModel):
